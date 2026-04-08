d19da52fda22a9c728817afe4997b44d09de84b3
rishu458 <vishanrandima12@gmail.com>
2026-04-08 23:22:58 +0530

    Password reset panel

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import 'tailwindcss/tailwind.css';
 import Register from './components/Register';
 import RegisterCSV from './components/csv';
+import PasswordReset from './components/change_password';
 
 
 function App() {
   return (
     <div className="App">
-      <RegisterCSV />
+      <PasswordReset />
+      
     </div>
   );
 }
